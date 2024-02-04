3adc5601d5cf1fd0c11dc752c5cebfb05e0f1619
AMKiasat <kiasatamirm@gmail.com>
2024-02-04 15:36:44 +0330

    Adding Boston housing dataset and preprocessing it

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import csv
+import numpy as np
+
+def reading_files(filename):
+    list1 = []
+    # list2 = []
+    file = open(filename)
+    for line in file:
+        if line.__contains__('crim'):
+            continue
+        values = [float(value.strip('"')) for value in line.strip().split(',')]
+        # list2.append(scaled.pop())
+        list1.append(values)
+        print(values)
+    data = np.array(list1, dtype=int)
+    # label = np.array(list2)
+    return data
+
 
 if __name__ == '__main__':
-    print("hello world")
+    reading_files('BostonHousing.csv')
